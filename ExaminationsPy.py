9e40ff6c80a1d67638dc215003d2d3ded6bf94a2
Richard Beattie <richard.beattie@aol.co.uk>
2019-12-19 20:09:07 +0000

    Fetches urls for all exam materials

--- ExaminationsPy.py
+++ ExaminationsPy.py
@@ -3,88 +3,100 @@ import examinations_environment
 
 class ExamMaterial():
 
-    def __init__(self, type, year, exam, subject, level):
+    def __init__(self, type, exam, year, subject, level, title, url):
         self.type = type
         self.year = year
         self.exam = exam
         self.subject = self.__subjectId(subject)
         self.level = level
-        self.url = ""
-        self.title = ""
+        self.url = url
+        self.title = title
 
     def __subjectId(self, subjectName):
         subjects = examinations_environment.EXAMINATION_SUBJECTS[self.exam]
-        return subjects[subjectName]
-
-    def __parsePage(self, content):
-        next = False
-        for line in iter(content.splitlines()):
-            if self.level in str(line):
-                if "IV" not in str(line):
-                    title = str(line).split(">")[1]
-                    title = title.split("<")[0]
-                    next = True
-            if next and "href=?fp" in str(line):
-                url = str(line).split("href=")[1]
-                url = url.split(" ")[0]
-                return title, url
-        return False
-
-    def fetch(self):
-        post_data = {"MaterialArchive__noTable__sbv__ViewType": self.type, "MaterialArchive__noTable__sbv__YearSelect": self.year, "MaterialArchive__noTable__sbv__ExaminationSelect": self.exam, "MaterialArchive__noTable__sbv__SubjectSelect": self.subject}
-        post_data.update(examinations_environment.POST_DATA)
-        r = requests.post("https://www.examinations.ie/exammaterialarchive/index.php", data=post_data)
-        title, url = self.__parsePage(r.content)
-        self.url = "https://examinations.ie/exammaterialarchive/" + url
-        self.title = title
-        return True
+        try:
+            return subjects[subjectName]
+        except KeyError:
+            return subjectName
 
 class Examinations():
 
-    def __init__(self, exam = ""):
+    def __init__(self, exam):
         self.exam = exam
 
-    def query(self, type = "", year = "", exam = "", subject = ""):
-        post_data = {"MaterialArchive__noTable__sbv__ViewType": type, "MaterialArchive__noTable__sbv__YearSelect": year, "MaterialArchive__noTable__sbv__ExaminationSelect": exam}
+    def query(self, type = "", exam = "", subject = "", year = ""):
+        post_data = {"MaterialArchive__noTable__sbv__ViewType": type, "MaterialArchive__noTable__sbv__YearSelect": year, "MaterialArchive__noTable__sbv__ExaminationSelect": exam, "MaterialArchive__noTable__sbv__SubjectSelect": subject}
         post_data.update(examinations_environment.POST_DATA)
         r = requests.post("https://www.examinations.ie/exammaterialarchive/index.php", data=post_data)
         return r
 
-    def __parseOptions(self, content, delimeter):
+    def __parseOptions(self, content, delimeter, value = False):
         nextLines = False
         options = []
         for line in iter(content.splitlines()):
             if delimeter in str(line):
-                elements = str(line).split(">")
+                elements = str(line).split("<")
                 for element in elements:
-                    if "</" in element and element.split("</")[0] != "":
-                        toAppend = element.split("</")[0]
+                    if "option" in element and element.split(">")[1] != "":
+                        toAppend = element.split(">")[1]
                         toAppend = toAppend.replace("\\x96 ", "") #Special characters present in some subjects
-                        options.append(toAppend)
+                        if(value):
+                            id = element.split('="')[1].split('"')[0]
+                            options.append([toAppend, id])
+                        else:
+                            options.append(toAppend)
                 break
         return options
 
-    def years(self, exam = ""):
-        exam = self.exam if exam == "" else exam
-        if(exam == ""):
+    def __extractMaterials(self, content):
+        titles = []
+        urls = []
+        next = False
+        for line in iter(content.splitlines()):
+            line = str(line)
+            if "class='materialbody'" in line and "</TD>" in line:
+                title = line.split(">")[1].split("<")[0]
+                titles.append(title)
+                next = True
+            if next and "<!--" not in line and "<a href=" in line:
+                url = line.split('href=')[1].split(' ')[0]
+                urls.append(url)
+                next = False
+        return titles, urls
+
+    def subjectId(self, subjectName):
+        subjects = examinations_environment.EXAMINATION_SUBJECTS[self.exam]
+        try:
+            return subjects[subjectName]
+        except KeyError:
             return False
+
+    def years(self):
         r = self.query("exampapers")
         years = self.__parseOptions(r.content, "[Select Year]")
         return years[2:]
 
-    def subjects(self, exam = ""):
-        exam = self.exam if exam == "" else exam
-        if(exam == ""):
-            return False
-        r = self.query("exampapers", 2019, exam)
-        subjects = self.__parseOptions(r.content, "[Select Subject]")
+    def subjects(self):
+        r = self.query("exampapers", self.exam, year = 2019)
+        subjects = self.__parseOptions(r.content, "[Select Subject]", True)
         return subjects[2:]
 
-    def papers(self, subject, level, exam = ""):
-        exam = self.exam if exam == "" else exam
-        if(exam == ""):
-            return False
-        papers = []
-        for year in self.years(exam):
-            papers.append(ExamMaterial("exampapers", int(year), exam, subject, level))
-        return papers
+    def __materials(self, type, subject, year = None, level = None):
+        materials = []
+        years = self.years() if year == None else [year]
+        subject = self.subjectId(subject)
+        for year in years:
+            r = self.query(type, self.exam, subject, year)
+            paperTitles, paperUrls = self.__extractMaterials(r.content)
+            for i, title in enumerate(paperTitles):
+                if level == None or level in title:
+                    url = "https://www.examinations.ie/exammaterialarchive/" + paperUrls[i] if "https" not in paperUrls[i] else paperUrls[i]
+                    material = ExamMaterial(type, self.exam, year, subject, level, title, url)
+                    materials.append(material)
+        return materials
+
+    def papers(self, subject, year = None, level = None):
+        return self.__materials("exampapers", subject, year, level)
+
+    def schemes(self, subject, year = None, level = None):
+        return self.__materials("markingschemes", subject, year, level)
--- main.py
+++ main.py
@@ -2,33 +2,58 @@ import ExaminationsPy as ex
 import pymysql.cursors
 import pdb
 
-levels = ["Higher Level", "Ordinary Level"]
+def setup_tables():
+    papersSql = "CREATE Table IF NOT EXISTS papers (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, subject VARCHAR(200) NOT NULL, exam VARCHAR(200) NOT NULL, year INT(20) UNSIGNED, level VARCHAR(100) NOT NULL, url VARCHAR(400) NOT NULL)"
+    schemesSql = "CREATE Table IF NOT EXISTS schemes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, subject VARCHAR(200) NOT NULL, exam VARCHAR(200) NOT NULL, year INT(20) UNSIGNED, level VARCHAR(100) NOT NULL, url VARCHAR(400) NOT NULL)"
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(papersSql)
+            cursor.execute(schemesSql)
+        connection.commit()
+        return True
+    except:
+        return False
 
-def add_papers(connection, exam):
-    subjects = Examinations.subjects(exam)
+def insert_materials(connection, table, materials):
+    for material in materials:
+        with connection.cursor() as cursor:
+            sql = "Insert INTO " + table + " (year, subject, exam, level, url) VALUES ({0}, '{1}', '{2}', '{3}', '{4}')".format(material.year, material.subject, material.exam, material.title, material.url)
+            cursor.execute(sql)
+        connection.commit()
+
+def subject_exists(connection, subject_id, exam):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT id FROM papers WHERE subject = {0} AND exam = '{1}' LIMIT 1".format(subject_id, exam)
+            cursor.execute(sql)
+            results = cursor.fetchall()
+        if(len(results) > 0):
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def add_materials(connection, exam):
+    Examinations = ex.Examinations(exam)
+    subjects = Examinations.subjects()
     for subject in subjects:
-        pdb.set_trace()
-        table = exam + "_papers_"  + subject.replace(" ", "_").lower()
-        try:
-            with connection.cursor() as cursor:
-                sql = "CREATE Table lc_papers_accounting (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, year INT(20) UNSIGNED, level VARCHAR(100) NOT NULL, type VARCHAR(200) NOT NULL, url VARCHAR(400) NOT NULL)"
-                cursor.execute(sql)
-            connection.commit()
-            for level in levels:
-                papers = Examinations.papers(exam = "lc", subject = subject, level = level)
-                for paper in papers:
-                    paper.fetch()
-                    with connection.cursor() as cursor:
-                        cursor.execute("Insert INTO " + table + " (year, level, type, url) VALUES ({0}, '{1}', '{2}', '{3}')".format(paper.year, paper.title, "exampapers", paper.url))
-                    connection.commit()
-        except:
-            print("Error")
-
-def main():
-    connection = pymysql.connect(host = "localhost", user = "root", password = "", db = "LearnFocused", charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-    add_papers(connection, "lc")
-    connection.close()
+        if(not subject_exists(connection, subject[1], exam)):
+            print("Now adding: " + exam + "'s " + subject[0])
+            papers = Examinations.papers(subject[0])
+            insert_materials(connection, "papers", papers)
+            schemes = Examinations.schemes(subject[0])
+            insert_materials(connection, "schemes", schemes)
+        else:
+            print("Exists already")
 
 if __name__ == "__main__":
-    Examinations = ex.Examinations()
-    main()
+    #Setup our interfaces
+    connection = pymysql.connect(host = "localhost", user = "root", password = "", db = "learnfocused", charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    #Add tables if they do not exist
+    if(setup_tables()):
+        #Add materials for each exam
+        exams = ["jc", "lc", "lb"]
+        for exam in exams:
+            add_materials(connection, exam)
+        connection.close()
