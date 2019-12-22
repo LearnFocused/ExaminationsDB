8e03c2572f79cf5521ab5b0c35938831aab41efc
Richard Beattie <richard.beattie@aol.co.uk>
2019-12-22 17:29:44 +0000

    Creates subjects table

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pymysql.cursors
 import pdb
 
 def setup_tables():
+    subjectsSql = "CREATE Table IF NOT EXISTS subjects (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, exam VARCHAR(200) NOT NULL, subject INT(11) NOT NULL, name VARCHAR(100) NOT NULL)"
     papersSql = "CREATE Table IF NOT EXISTS papers (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, subject VARCHAR(200) NOT NULL, exam VARCHAR(200) NOT NULL, year INT(20) UNSIGNED, level VARCHAR(100) NOT NULL, url VARCHAR(400) NOT NULL)"
     schemesSql = "CREATE Table IF NOT EXISTS schemes (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, subject VARCHAR(200) NOT NULL, exam VARCHAR(200) NOT NULL, year INT(20) UNSIGNED, level VARCHAR(100) NOT NULL, url VARCHAR(400) NOT NULL)"
     try:
         with connection.cursor() as cursor:
+            cursor.execute(subjectsSql)
             cursor.execute(papersSql)
             cursor.execute(schemesSql)
         connection.commit()
@@ -20,6 +22,12 @@ def insert_materials(connection, table, materials):
             sql = "Insert INTO " + table + " (year, subject, exam, level, url) VALUES ({0}, '{1}', '{2}', '{3}', '{4}')".format(material.year, material.subject, material.exam, material.title, material.url)
             cursor.execute(sql)
         connection.commit()
+
+def insert_subject(connection, exam, subject, name):
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO subjects (exam, subject, name) VALUES ('{0}', {1}, '{2}')".format(exam, subject, name)
+        cursor.execute(sql)
+    connection.commit()
 
 def subject_exists(connection, subject_id, exam):
     try:
@@ -38,8 +46,12 @@ def add_materials(connection, exam):
     Examinations = ex.Examinations(exam)
     subjects = Examinations.subjects()
     for subject in subjects:
-        if(not subject_exists(connection, subject[1], exam)):
+        # not subject_exists(connection, subject[1], exam)
+        if(True):
             print("Now adding: " + exam + "'s " + subject[0])
+            #Add the subject to subjects table
+            insert_subject(connection, exam, subject[1], subject[0])
+            #Add all the papers and marking schemes
             papers = Examinations.papers(subject[0])
             insert_materials(connection, "papers", papers)
             schemes = Examinations.schemes(subject[0])
@@ -52,8 +64,12 @@ if __name__ == "__main__":
     connection = pymysql.connect(host = "localhost", user = "root", password = "", db = "learnfocused", charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
     #Add tables if they do not exist
     if(setup_tables()):
+        #Add all the subjects
+        print("Hello")
         #Add materials for each exam
         exams = ["jc", "lc", "lb"]
         for exam in exams:
             add_materials(connection, exam)
         connection.close()
+    else:
+        print("Error creating tables")
